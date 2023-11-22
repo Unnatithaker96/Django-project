bf750cece561f30d9bfb56f4ecfb3c335ee0e7c6
martin <martin.evenbom@gmail.com>
2023-11-22 15:44:37 +0100

    orders model

---
+++ webapp/models/orders.py
@@ -0,0 +1,17 @@
+from django.db import models
+import datetime
+
+class Order(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.IntegerField(default=1)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def placeOrder(self):
+        self.save()
+
+    @staticmethod
+    def get_order_by_customer(customer_id):
+        return Order.objects.filter(customer=customer_id).order_by('-date')
