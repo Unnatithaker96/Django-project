6df972786f70ebf86132736981d6fcf3c6b6bf3d
martin <martin.evenbom@gmail.com>
2023-11-23 17:15:04 +0100

    cart view

---
+++ webapp/views/cart.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import View
+from webapp.models import Products
+
+class Cart(View):
+    def get(self, request):
+        ids = list(request.session.get('cart').keys())
+        products = Products.get_products_by_id(ids)
+        print(products)
+        return render(request, 'cart,html', {'products': products})
