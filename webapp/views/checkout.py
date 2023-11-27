94c99314209e2dea266255fbe1f24fcdf62af64c
martin <martin.evenbom@gmail.com>
2023-11-27 16:27:43 +0100

    updated view.checkout

--- webapp/views/checkout.py
+++ webapp/views/checkout.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 
 #from django.contrib.auth.hashers import check_password
-from store.models.customer import User
+from webapp.models.customer import User
 from django.views import View
 
-from store.models.product import Product
-from store.models.orders import Order
+from webapp.models.product import Product
+from webbapp.models.orders import Order
 
 class Checkout(View):
     def post(self, request):
