e058e5180d95625d1b10309f0f94d3d5cb701cf3
Unnati Thaker <148350236+Unnatithaker96@users.noreply.github.com>
2023-11-27 12:09:12 +0100

    register page

---
+++ project_webshop/webapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from .forms import LoginForm, RegisterForm
+
+def sign_up(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        return render(request, 'users/register.html', { 'form': form})  
+    
+   
+   
+    if request.method == 'POST':
+        form = RegisterForm(request.POST) 
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You have singed up successfully.')
+            login(request, user)
+            return redirect('posts')
+        else:
+            return render(request, 'users/register.html', {'form': form})
